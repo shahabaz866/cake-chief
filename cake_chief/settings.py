6d1e31b5867d28c00233c91bb416252e689c69f4
Muhammed shahabas VP <shahabaz866@gmail.com>
2025-03-13 09:42:28 +0530

    Task: .env updation

--- cake_chief/settings.py
+++ cake_chief/settings.py
@@ -36,11 +36,8 @@ INSTALLED_APPS = [
 ]
 
 CSRF_COOKIE_SECURE = True
-CSRF_TRUSTED_ORIGINS = [
-    "http://localhost",
-    "http://127.0.0.1",
-"https://cakecheif.sbs","http://www.cakecheif.sbs","http://43.204.112.140","http://3.95.8.35","https://3.95.8.35"
-]
+CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS', default=[])
+
 
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_USERNAME_REQUIRED = False
@@ -164,3 +161,19 @@ SOCIALACCOUNT_PROVIDERS = {
 SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'allauth': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+}
